787a270a45096ac9f7e5322d5e899712e7ac37c6
Vasily Himchenko <White_Scorpion@inbox.lv>
2024-01-17 20:37:23 +0200

    Changes: Binding & INotifyProperyChanged

---
+++ InfoBindingApp/FMain.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace InfoBindingApp
+{
+    partial class FMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbData = new ListBox();
+            bnAdd = new Button();
+            bnUpdate = new Button();
+            bnDelete = new Button();
+            dgvData = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvData).BeginInit();
+            SuspendLayout();
+            // 
+            // lbData
+            // 
+            lbData.FormattingEnabled = true;
+            lbData.ItemHeight = 25;
+            lbData.Location = new Point(12, 12);
+            lbData.Name = "lbData";
+            lbData.Size = new Size(422, 654);
+            lbData.TabIndex = 0;
+            // 
+            // bnAdd
+            // 
+            bnAdd.Location = new Point(440, 12);
+            bnAdd.Name = "bnAdd";
+            bnAdd.Size = new Size(112, 34);
+            bnAdd.TabIndex = 1;
+            bnAdd.Text = "Add";
+            bnAdd.UseVisualStyleBackColor = true;
+            bnAdd.Click += bnAdd_Click;
+            // 
+            // bnUpdate
+            // 
+            bnUpdate.Location = new Point(440, 52);
+            bnUpdate.Name = "bnUpdate";
+            bnUpdate.Size = new Size(112, 34);
+            bnUpdate.TabIndex = 2;
+            bnUpdate.Text = "Update";
+            bnUpdate.UseVisualStyleBackColor = true;
+            bnUpdate.Click += bnUpdate_Click;
+            // 
+            // bnDelete
+            // 
+            bnDelete.Location = new Point(440, 92);
+            bnDelete.Name = "bnDelete";
+            bnDelete.Size = new Size(112, 34);
+            bnDelete.TabIndex = 3;
+            bnDelete.Text = "Delete";
+            bnDelete.UseVisualStyleBackColor = true;
+            bnDelete.Click += bnDelete_Click;
+            // 
+            // dgvData
+            // 
+            dgvData.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvData.Location = new Point(556, 208);
+            dgvData.Name = "dgvData";
+            dgvData.RowHeadersWidth = 62;
+            dgvData.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvData.Size = new Size(1211, 502);
+            dgvData.TabIndex = 4;
+            // 
+            // FMain
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(2017, 881);
+            Controls.Add(dgvData);
+            Controls.Add(bnDelete);
+            Controls.Add(bnUpdate);
+            Controls.Add(bnAdd);
+            Controls.Add(lbData);
+            Name = "FMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dgvData).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox lbData;
+        private Button bnAdd;
+        private Button bnUpdate;
+        private Button bnDelete;
+        private DataGridView dgvData;
+    }
+}
---
+++ InfoBindingApp/FMain.cs
@@ -0,0 +1,113 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace InfoBindingApp
+{
+    public partial class FMain : Form
+    {
+        private static string[] names = new[]
+            { "Vasiliy", "Bogdan", "Anastacia", "Oleg", "Ivan", "Nicolay", "Olga", "Varvara" };
+
+        private static string[] surnames = new[]
+            { "Ivanov", "Petrov", "Sidorov", "Sologub", "Mikul", "Krasnov", "Orehov", "Tormoz" };
+
+        private readonly Random _rand;
+        private readonly BindingList<Person> _list;
+
+        public FMain()
+        {
+            InitializeComponent();
+            this._rand = new Random();
+            this._list = new BindingList<Person>();
+
+            lbData.DataSource = this._list;
+            dgvData.DataSource = this._list;
+        }
+
+        private string GenRandName()
+        {
+            return names[this._rand.Next(names.Length)];
+        }
+
+        private string GenRandSurname()
+        {
+            return surnames[this._rand.Next(surnames.Length)];
+        }
+
+        private void bnAdd_Click(object sender, EventArgs e)
+        {
+            var person = new Person()
+            {
+                Name = this.GenRandName(),
+                Surname = this.GenRandSurname()
+            };
+
+            this._list.Add(person);
+        }
+
+        private void bnUpdate_Click(object sender, EventArgs e)
+        {
+            var selected = lbData.SelectedItem as Person;
+            selected.Name = this.GenRandName();
+        }
+
+        private void bnDelete_Click(object sender, EventArgs e)
+        {
+            var selected = lbData.SelectedItem as Person;
+            this._list.Remove(selected);
+        }
+    }
+
+    public class Person : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _surname;
+
+        public string Name
+        {
+            get => this._name;
+            set
+            {
+                this._name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return this._surname;
+            }
+            set
+            {
+                this._surname = value;
+                OnPropertyChanged(nameof(Surname));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Surname}";
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        #endregion
+    }
+}
